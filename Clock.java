393db52854a8c11775fcfb84a730bf23dadff004
ChampanaAchar <champana.xworkz@gmail.com>
2024-06-20 14:20:17 +0530

    Clock class

---
+++ Clock.java
@@ -0,0 +1,7 @@
+class Clock{
+  public static void main(String clock[]){
+   System.out.println(" clock, mechanical or electrical device other than a watch for displaying time. A clock is a machine in which a device that performs regular movements in equal intervals of time is linked to a counting mechanism that records the number of movements.");
+   System.out.println("clock is a device used to tell time. Moving hands on the face of a clock point to the current hour, minute, and second. A clock can be big enough to be in a giant tower. A clock can also be small enough to fit on a person's wrist, where it is called a watch. ");
+   System.out.println("The big hand is the minute hand and the small hand is the hour hand. When the minute hand is pointing to 12 it is O'clock. When the minute hand is pointing to the 6 it means it is half past. The time on this clock is half past 12 because the hour hand is pointing to just after 12 and the minute hand is pointing to 6. ");
+   }
+  }
